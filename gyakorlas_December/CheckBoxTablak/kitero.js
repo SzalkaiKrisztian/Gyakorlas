30f89b2ea9795e25435f6b49a9b16979e54e86f2
krisztian <szalkai.krisztian_2022c@bolyai-szakkozep.hu>
2025-12-01 22:57:32 +0100

    commentek

--- gyakorlas_December/CheckBoxTablak/kitero.js
+++ gyakorlas_December/CheckBoxTablak/kitero.js
@@ -81,12 +81,14 @@ jsForm.addEventListener("submit",function(e){
     const pl2Input = target.querySelector('#otodik')
 
     if(validate3Fields(helyInput,ag1Input,pl1Input)){
-        let seged = true
-        if(ag2Input.value){
-            if(validField(pl2Input,"kötelező ha adsz masik agazatot") == false){seged=false}
-        }else if(pl2Input.value){
-            if(validField(ag2Input,"kötelező ha adsz masik peldat") ==false){seged=false}
-        }   
+        //3 alap kötelező kész, de ha van 4. vagy 5. akkor kötelező a másik!
+        let seged = true//alapból lefut 3 kotelezővel mert true
+        if(ag2Input.value){//van e benne szoveg
+            if(validField(pl2Input,"kötelező ha adsz masik agazatot!") == false){seged=false}
+        }else if(pl2Input.value){// van e benne szöveg
+            if(validField(ag2Input,"kötelező ha adsz masik peldat!") ==false){seged=false}
+        }
+
         /**@type {string} */
         const helyString = helyInput.value
         /**@type {string} */
@@ -103,7 +105,7 @@ jsForm.addEventListener("submit",function(e){
         obj.pl1=pl1String
         pl2String==''? obj.pl2=undefined : obj.pl2=pl2String
         ag2String==''? obj.ag2=undefined : obj.ag2=ag2String
-        if(seged){
+        if(seged){//alapból true
             tableArr.push(obj)
             generateTableBody(tableArr)
             jsForm.reset()
